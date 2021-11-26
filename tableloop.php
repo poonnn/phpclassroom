cbf7f15bea28b2350b9028d9d8d95de0b8704b78
poonnn <73819265+poonnn@users.noreply.github.com>
2021-11-26 12:48:43 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -2,14 +2,16 @@
   <head>
   </head>
   <body>
-    <table border="1">
+    <table border = "1">
       <?php
-        for($i=0;$i<5;$i++){
-          echo "<tr>";
-          echo "<td>คอลัมน์</td>";
-          echo "</tr>";
+        for($i=0;$i<100;$i++){
+          echo"<tr>";
+            for($i=0;$i<6;$i++){
+          echo"<td>corlum1</td>";
+            }
+          echo"</tr>";
         }
-       ?>
+      ?>
     </table>
   </body>
 </html>
