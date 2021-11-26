138f006c7680bdff5aa01d554a83988b4e1aa809
poonnn <73819265+poonnn@users.noreply.github.com>
2021-11-26 14:45:31 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -3,8 +3,7 @@
   $data = json_decode($json);
 
   echo "<table border='1'>";
-         for($i=0;$i++){
-          echo"<tr>";
+         
   foreach ( $data[0] as $key => $val ){
     echo $key.":".$val."<br>";
   }
