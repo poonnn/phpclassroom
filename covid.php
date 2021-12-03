76836f3e01ff4563b34edae9aea93c6d8cb9dcf1
poonnn <73819265+poonnn@users.noreply.github.com>
2021-12-03 12:47:12 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -1,1 +1,19 @@
+<?php
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all%22);
+  $data = json_decode($json);
 
+  echo "<table border='1'>";
+
+ foreach ( $data[0] as $key => $val )
+ {
+    echo "<tr>";
+    echo "<td>";
+    echo  $key."<br>"; 
+    echo "</td>";
+    echo "<td>";
+    echo  $val;
+    echo "</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+?>
