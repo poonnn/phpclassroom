5bde7d4bc4c924da92a0243a7a26484d31f2691a
poonnn <73819265+poonnn@users.noreply.github.com>
2021-12-24 16:08:51 +0700

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -1,28 +1,30 @@
 <?php
-
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
-  
   echo "<table border='1'>";
   echo "<tr>";
-  echo "<td>"; 
+  echo "<td bgcolor='#66FFCC'>"; 
   echo "ลำดับ";
   echo "</td>"; 
-  echo "<td>"; 
+  echo "<td bgcolor='#66CCFF'>"; 
   echo "จังหวัด";
-  echo "</td>"; 
+  echo "</td>";
+  echo "<td bgcolor='#FFD700'>"; 
+  echo "ผู้ป่วยใหม่";
+  echo "</td>";
   echo "</tr>";
-
- foreach($data as $key=>$val){
+  foreach($data as $key=>$val){
     echo "<tr>";
-    echo "<td>"; 
+    echo "<td bgcolor='#66FFCC'>"; 
     echo $key+1;
     echo "</td>"; 
-    echo "<td>"; 
+    echo "<td bgcolor='#66CCFF'>"; 
     echo $val->province;
+    echo "</td>"; 
+    echo "<td bgcolor='#FFD700'>"; 
+    echo $val->new_case;
     echo "</td>"; 
     echo "</tr>";
   }
-
   echo "</table>";
 ?>
