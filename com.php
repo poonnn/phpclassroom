3509eaf854634434a1d857beceb8a335cdf1d60f
poonnn <73819265+poonnn@users.noreply.github.com>
2022-03-03 11:24:02 +0700

    Create com.php

---
+++ com.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css
+" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>chiangmai province website</title>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Sarabun
+"
+    />
+    <style>
+      body {
+        font-family : "sarabun";
+        background-color : #CCFFFF ;
+      }
+      #header{
+        height: 160px;
+        padding: 50px;
+        background-image: url("p.jpg");
+        background-size:100%;
+      }
+      #sidebar{
+        background-color: #f1f1f1;
+        padding: 0px;
+      }
+      #content{
+        padding: 10px;
+        background-color: gray;
+      }
+      #footer{
+        padding: 30px;
+        height: 200px;
+        background-color: pink;
+      }
+      
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        background-color: #f1f1f1;
+      }
+      li a {
+        display: block;
+        color: #000;
+        padding: 8px 16px;
+        text-decoration: none;
+      }
+      /* Change the link color on hover */
+      li a:hover {
+        background-color: #555;
+        color: white;
+      }
+      p{
+        margin-top: 10px;
+      }
+    </style>
+    
+  </head>
+  <body> 
+    <div class="container"> 
+      <div class="row">
+        <div class="col-12" id="header">
+          <center><h1>จังหวัดเชียงใหม่</h1></center>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3" id="sidebar"> 
+          <ul>
+            <li><a href="lswproject.php">หน้าแรก</a></li>
+            <li><a href="location.php">แหล่งท่องเที่ยว</a></li>
+            <li><a href="custom.php">ประเพณีและวัฒนธรรม</a></li>
+            <li><a href="food.php">อาหารประจำท้องถิ่น</a></li>
+            <li><a href="owner.php">ข้อมูลผู้จัดทำ</a></li>
+          </ul>
+        </div>
+        <div class="col-9" id="content"> 
+          <h4>ประเพณีและวัฒนธรรม</h4><br>
+          
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เมืองเชียงใหม่มีประวัติศาสตร์ที่ยาวนาน คนเชียงใหม่ได้สั่งสมวัฒนธรรมประเพณีสืบทอดมาจากบรรพบุรุษอย่างต่อเนื่อง โดยส่วนใหญ่มีความผูกพันกับพุทธศาสนาและความเชื่อดั้งเดิม ประเพณีที่สำคัญ ได้แก่<br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ปีใหม่เมือง(สงกรานต์)</b> จัดขึ้นระหว่างวันที่ 13-15 เมษายนของทุกปี เป็นประเพณีที่สำคัญและยิ่งใหญ่ของชาวเชียงใหม่ แบ่งเป็นวันที่ 13 เป็นวันสังขารล่อง มีขบวนแห่พระพุทธสิหิงค์ และพิธีสรงน้ำพระ วันที่ 14 เป็นวันเน่า ชาวบ้านจะเตียมข้าวของไปวัดและรดน้ำดำหัวผู้ใหญ่วันต่อไป และวันที่ 15 เมษายน ประเพณีรดน้ำดำหัวผู้ใหญ่ และมีการเล่นสาดน้ำตลอดช่วงเทศกาล เปรียบเหมือนสงครามเล่นสาดน้ำสุดมันส์สำหรับชาวไทยและต่างประเทศ นอกจากนี้ยังมีขบวนแห่นางสงกรานต์ การก่อเจดีย์ทราย รดน้ำดำหัวขอพรผู้ใหญ่ และที่ห้ามพลาดคือการสรงน้ำพระพุทธสิหิงค์ พระพุทธรูปองค์สำคัญของชาวเชียงใหม่ที่จะอัญเชิญขึ้นบนขบวนแห่ออกมาจากวัดพระสิงห์วรมหาวิหารในวันมหาสงกรานต์<br>
+          <center>
+            <img src="songkran.jpg" width = "360px" class="img-fluid rounded"><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ประเพณียี่เป็ง</b> งานประเพณีปล่อยโคมลอยสู่ท้องฟ้าที่อลังการที่สุดของเมืองไทย จัดในช่วงเทศกาลลอยกระทงช่วงเดือนพฤศจิกายน มีการตกแต่งบ้านเรือนและสถานที่ต่างๆ ด้วยโคมชนิดต่างๆ มีการปล่อยโคมลอย มีการลอยกระทง พร้อมชมขบวนแห่โคมยี่เป็ง ขบวนแห่กระทง การจุดพลุดอกไม้ไฟ การประกวดกระทง หรือจะร่วมเชียร์การประกวดหนูน้อยนพมาศ การประกวดเทพีและเทพบุตรยี่เป็งเชียงใหม่ รวมไปถึงชมการแสดงทางวัฒนธรรมอีกมากมาย<br>
+          <center>
+            <img src="yebang.jpg" width = "360px" class="img-fluid rounded"><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ประเพณีเข้าอินทขิล</b> งานทำบุญบูชาเสาอินทขิลหรือเสาหลักเมืองซึ่งจะจัดขึ้นในช่วงปลายพฤษภาคมหรือต้นมิถุนายน ประชาชนจะนำดอกไม้ธูปเทียนใส่ขันหรือถาดมาถวายบูชาเสาอินทขิล นอกจากนี้ในพิธีจะมีการอัญเชิญพระพุทธรูปพระเจ้าฝนแสนห่ามาเป็นองค์ประธานและมีการสวดคาถาอินทขิลโดยหมู่พระสงฆ์<br>
+          <center>
+            <img src="intakit.jpg" width = "360px" class="img-fluid rounded"><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•เทศกาลร่มบ่อสร้าง</b> จัดขึ้นในช่วงเดือนมกราคมหลังจากจบงานฤดูหนาวเชียงใหม่ได้ไม่นาน เป็นการจัดงานในลักษณะถนนคนเดินที่มีการออกร้านและตกแต่งบ้านเรือนด้วยร่มพื้นเมืองสีสันสวยงามตั้งแต่ทางเข้าหมู่บ้านไปจนถึงท้ายหมู่บ้านยาวกว่าหนึ่งกิโลเมตร นอกจากนี้ยังมีขบวนแห่รถประดับร่ม การประกวดธิดาบ่อสร้าง และการแสดงวัฒธรรมพื้นบ้าน<br>
+          <center>
+            <img src="rom.jpg" width = "360px" class="img-fluid rounded"><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•มหกรรมไม้ดอกไม้ประดับ</b> โด่งดังมาจากการจัดงานมหกรรมพืชสวนโลกเฉลิมพระเกียรติฯ งานเทศกาลชมสวน หรือ Flora Festival นี้จะจัดขึ้นในช่วงเดือนธันวาคมถึงกุมภาพันธ์ มีการจัดแสดงสวนสวยในรูปแบบต่างๆ ที่ตกแต่งด้วยไม้ประดับและดอกไม้เมืองหนาวนานาพรรณจากโครงการหลวงกว่า 20 โครงการ ที่มีชื่อเสียงที่สุดคืออุทยานกล้วยไม้และอุโมงค์ดอกไม้ที่เป็นจุดถ่ายภาพยอดนิยมของนักท่องเที่ยวส่วนใหญ่<br>
+          <center>
+            <img src="dokmai.jpg" width = "360px" class="img-fluid rounded"><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•งานไม้แกะสลักบ้านถวาย</b> จัดขึ้นในเดือนมกราคม ที่หมู่บ้านถวาย อำเภอหางดง มีการจำหน่ายและสาธิตการแกะสลักไม้ และหัตถกรรมพื้นบ้าน<br>
+          <center>
+            <img src="carpenter.jpg" width = "360px" class="img-fluid rounded"><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ประเพณีแห่ไม้ค้ำโพธิ์</b> จัดขึ้นในเดือนเมษายน ในวันที่ 15 เป็นต้นไป ของทุกปี ที่บริเวณตัวเมืองจอมทอง มีขบวนรถจากชุมชน ห้างร้าน กลุ่มต่างๆ กว่า 40 ขบวน แห่ไปตามเมืองจอมทอง อำเภอจอมทอง จนถึง วัดพระธาตุศรีจอมทองวรวิหาร เป็นประเพณีที่สืบทอดกันมานานกว่า 200 ปี ตามตำนานเกิดขึ้นที่อำเภอเภอจอมทอง ถือเป็นแห่งแรกของประเทศไทยและแห่งเดียวในโลก ประเพณีแห่ไม้ค้ำโพธิ์ กลายเป็นต้นแบบของการแห่ไม้ค้ำสะหลีของชาวล้านนา จนได้รับความนิยมไปทั่วภาคเหนือ และเป็นประเพณีที่เริ่มมีชื่อเสียงโด่งดัง และได้รับความนิยมอย่างมาก<br>
+          <center>
+            <img src="tree.jpg" width = "360px" class="img-fluid rounded"><br>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ประเพณีเดินขึ้นดอยสุเทพ</b> งานบุญที่จะจัดขึ้นในช่วงวันวิสาขบูชาประมาณปลายเดือนพฤษภาคม มีขบวนแห่น้ำสรงและผ้าไตรพระราชทานจากลานอนุสาวรีย์ครูบาศรีวิชัยขึ้นไปยังวัดพระธาตุดอยสุเทพ จากนั้นจะมีพิธีทำบุญตักบาตรและพิธีถวายน้ำสรงพระราชทานแด่องค์พระบรมธาตุดอยสุเทพ และพิธีเวียนเทียนรอบพระบรมธาตุฯ
+           <center>
+            <img src="mong.jpg" width = "360px" class="img-fluid rounded"><br>
+        </div>
+      </div>
+       <div class="row">
+        <div class="col-12" id="footer"> 
+         <center>
+            <h3>ผลงานนักเรียน</h3>
+            <hr />
+            <p>รายวิชา WebApplication</p>
+            <p>โรงเรียนหล่มสักวิทยาคม</p>
+          </center>
+        </div>
+      </div>
+    </div>  
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js
+" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
